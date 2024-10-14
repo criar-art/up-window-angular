6946e5bab7f39760ec9a9fb996b2aa591dae980a
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-10-14 12:35:32 -0300

    adjustment class animation in window

--- projects/up-window-angular/src/lib/up-window-angular.component.ts
+++ projects/up-window-angular/src/lib/up-window-angular.component.ts
@@ -18,9 +18,9 @@ export class UpWindowAngularComponent implements OnInit {
   @Input() subtitle: string = 'Default Subtitle';
   @Input() size: string = 'medium';
   @Input() class: string | undefined;
-
   @Input() isOpen: WritableSignal<boolean> = signal(false);
   @Input() animation: string = 'fade';
+  closingAnimation: boolean = false;
 
   ngOnInit(): void {}
 
@@ -29,16 +29,21 @@ export class UpWindowAngularComponent implements OnInit {
   }
 
   closeWindow() {
-    const upWindowElement = document.querySelector('.up-window');
-    if (upWindowElement) {
-      upWindowElement.classList.add(`${this.animation}-out`);
-      setTimeout(() => {
-        this.isOpen.set(false);
-        upWindowElement.classList.remove(`${this.animation}-out`);
-      }, 300);
-    }
+    this.closingAnimation = true;
+
+    setTimeout(() => {
+      this.isOpen.set(false);
+      this.closingAnimation = false;
+    }, 300);
   }
 
+  getClass() {
+    return {
+      ...(this.class ? { [this.class]: true } : {}),
+      [this.animation]: !this.closingAnimation,
+      [`${this.animation}-out`]: this.closingAnimation,
+    };
+  }
 
   onConfirm() {
     console.log('Confirmed!');
