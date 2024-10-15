237578ed64e9de76381a2ff52fb6ee0c634d1741
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-10-14 23:33:29 -0300

    refactory and slipt files scss

--- projects/up-window-angular/src/lib/up-window-angular.component.ts
+++ projects/up-window-angular/src/lib/up-window-angular.component.ts
@@ -16,7 +16,12 @@ import {
 @Component({
   selector: 'up-window-angular',
   templateUrl: './up-window-angular.component.html',
-  styleUrls: ['./up-window-angular.component.scss'],
+  styleUrls: [
+    './up-window-angular.variables.scss',
+    './up-window-angular.animation.scss',
+    './up-window-angular.component.scss',
+    './up-window-angular.btn.scss',
+  ],
   encapsulation: ViewEncapsulation.None,
 })
 export class UpWindowAngularComponent implements OnInit, OnDestroy {
