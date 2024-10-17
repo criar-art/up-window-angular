362e67f16d94376ba0f817fb352318d57ba733f6
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-10-17 17:13:09 -0300

    add ChangeDetectorRef

--- projects/up-window-angular/src/lib/up-window-angular.component.ts
+++ projects/up-window-angular/src/lib/up-window-angular.component.ts
@@ -11,6 +11,7 @@ import {
   ElementRef,
   ViewChild,
   effect,
+  ChangeDetectorRef,
 } from '@angular/core';
 
 @Component({
@@ -55,7 +56,7 @@ export class UpWindowAngularComponent implements OnInit, OnDestroy {
 
   @ViewChild('modal') modal!: ElementRef;
 
-  constructor() {
+  constructor(private cdr: ChangeDetectorRef) {
     effect(() => {
       if (this.isOpen()) {
         this.addModalToBody();
@@ -86,6 +87,7 @@ export class UpWindowAngularComponent implements OnInit, OnDestroy {
           this.focusableElements[this.focusableElements.length - 1];
       }
     }
+    this.cdr.detectChanges();
   }
 
   addModalToBody() {
