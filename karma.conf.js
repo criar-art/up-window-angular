fbd3971d4553b7ffa42fcac988972dc0388f0d0d
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-10-17 18:30:57 -0300

    update karma config

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,3 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
-
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -14,10 +11,7 @@ module.exports = function (config) {
     ],
     client: {
       jasmine: {
-        // you can add configuration options for Jasmine here
-        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
-        // for example, you can disable the random execution with `random: false`
-        // or set a specific seed with `seed: 4321`
+        // Additional Jasmine configuration options can be added here
       },
     },
     jasmineHtmlReporter: {
@@ -36,7 +30,7 @@ module.exports = function (config) {
     customLaunchers: {
       ChromeHeadlessCI: {
         base: 'ChromeHeadless',
-        flags: ['--no-sandbox', '--disable-gpu']
+        flags: ['--no-sandbox', '--disable-gpu', '--disable-software-rasterizer']
       }
     },
     restartOnFileChange: true
